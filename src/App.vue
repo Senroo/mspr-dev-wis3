8cbc84eb273cc25856aa6ff45c9f2fbeea112916
Senroo <sengerromain75@gmail.com>
2021-01-05 11:27:33 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <nav class="main-nav">
+      <div class="logo">
+        <img width="20%" src="./assets/logo.png" />
+      </div>
+      <Burger></Burger>
+    </nav>
+
+    <Sidebar>
+      <ul class="sidebar-panel-nav">
+        <li><a href="/">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </Sidebar>
+    <Slider/>
+
+    <div class="container">
+      <div class="artiste">
+        <p>Oui</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Burger from "./components/menu/burger.vue";
+import Sidebar from "./components/menu/sidebar.vue";
+import Slider from"./components/artites/slider.vue";
+export default {
+  name: "app",
+  components: {
+    Burger,
+    Sidebar,
+    Slider,
+  },
+};
+</script>
+<style>
+html {
+  height: 100%;
+  overflow: hidden;
+}
+
+body {
+  border: 0;
+  margin: 0;
+  padding: 0;
+  font-family: "Lato";
+  height: 100%;
+  background: rgb(101, 31, 87);
+  background: linear-gradient(
+    45deg,
+    rgba(101, 31, 87, 1) 0%,
+    rgba(225, 113, 87, 1) 48%,
+    rgba(249, 248, 113, 1) 100%
+  );
+}
+
+.logo {
+  align-self: center;
+  color: #fff;
+  font-weight: bold;
+  font-family: "Lato";
+}
+
+.main-nav {
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 0.8rem;
+}
+
+ul.sidebar-panel-nav {
+  list-style-type: none;
+}
+
+ul.sidebar-panel-nav > li > a {
+  color: #fff;
+  text-decoration: none;
+  font-size: 1.5rem;
+  display: block;
+  padding-bottom: 0.5em;
+}
+</style>
---
+++ src/components/artites/slider.vue
@@ -0,0 +1,38 @@
+<template>
+<div id="slider">
+    <pre>{{info}}</pre>
+</div>
+  
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return{
+            artist:[],
+        }
+    },
+ mounted () {
+var options = {
+  method: 'GET',
+  url: 'https://theaudiodb.p.rapidapi.com/artist-mb.php',
+  params: {i: '3kdshn1asZHr5OgvBIVHnmQsD_mU5j6m'},
+  headers: {
+    'x-rapidapi-key': '16f13d8468msh4a621be92a240a3p17b32ejsn41ba33443dad',
+    'x-rapidapi-host': 'theaudiodb.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+ }}
+</script>
+
+<style>
+
+</style>
